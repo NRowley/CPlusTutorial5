e3340d04164f8ff9fda514599d9e2c73c84721d2
NRowley <nicholas.ryan.rowley@gmail.com>
2023-11-13 13:40:54 -0600

    Function Overloading

--- Tutorial5/Source.cpp
+++ Tutorial5/Source.cpp
@@ -3,36 +3,65 @@
 using namespace std;
 
 void ChangeStr(string& str);
-
+void Print(string str);
+void Print(int i);
+void Print(string str1, string str2);
+void Print(int i, string str);
 int main()
 {
 	//References////////////////////////////
 
-	/*string myStr = "Monk";
+	//string myStr = "Monk";
 
-	string& myRef = myRef;
+	//string& myRef = myRef;
 
-	cout << myRef << endl;
-	cout << myRef << endl;
+	//cout << myRef << endl;
+	//cout << myRef << endl;
 
-	myRef += " Fighter";
+	//myRef += " Fighter";
 
-	cout << myRef << endl;
-	cout << myRef << endl;
+	//cout << myRef << endl;
+	//cout << myRef << endl;
 
-	ChangeStr(myStr);
+	//ChangeStr(myStr);
 
-	cout << myStr << endl;*/
+	//cout << myStr << endl;
 
 	
 	
 	//Function Overloading////////////////////
+	int myInt = 1;
+	string myStr = "Killian";
+	string myStr2 = "Kaite";
 
+	Print("Hello World!");
+	Print(3);
+	Print("String1", "String2");
 
+	Print(myInt);
+	Print(myStr);
+	Print(myStr, myStr2);
+	Print(myInt, myStr);
 
 	system("pause");
 }
 
 void ChangeStr(string& str) {
 	str += "!";
+}
+
+void Print(string str) {
+	cout << str << endl;
+}
+
+void Print(int i) {
+	cout << i << endl;
+}
+
+void Print(string str1, string str2) {
+	cout << str1 << " " << str2 << endl;
+}
+
+void Print(int i, string str) {
+	cout << i << " " << str << endl;
 }
